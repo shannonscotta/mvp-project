7a2c652f7e3a48273c8561d72444f71773ea4d73
Scott Shannon <scott.a.shannon@outlook.com>
2023-10-19 16:14:50 -0700

    add delete request

--- public/app.js
+++ public/app.js
@@ -1,32 +1,56 @@
 const form = document.querySelector(".number-form");
+const thingies = document.querySelector(".thingies");
+const numInput = document.querySelector("input[name='num']");
+
+function createThingElement(thing) {
+  const p = document.createElement("p");
+  p.addEventListener("click", () => {
+    fetch(`/things/${thing.id}`, {
+      method: "DELETE",
+    }).then(() => {
+      p.remove();
+    });
+  });
+  p.innerText = thing.num;
+
+  return p;
+}
+
+function getThings() {
+  fetch("/things")
+    .then((response) => response.json())
+    .then((things) => {
+      thingies.innerText = "";
+      for (let thing of things) {
+        const element = createThingElement(thing);
+        thingies.append(element);
+      }
+    });
+}
+
+getThings();
 
 form.addEventListener("submit", (event) => {
+  // Prevent form from trying to auto-submit.
   event.preventDefault();
 
+  // Get data in the form.
   const formData = new FormData(event.target);
   const num = formData.get("num");
 
   fetch("/things", {
     method: "POST",
+    // We must stringify the body, because fetch won't do it for us.
     body: JSON.stringify({ num }),
     headers: {
+      // We must include this, or express doesn't know how to parse the body.
       "Content-Type": "application/json",
     },
   })
     .then((response) => response.json())
     .then((thing) => {
-      console.log("thing created", thing);
+      numInput.value = "";
+      getThings();
+      // figure out what to do here
     });
 });
-
-// fetch("/things")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((things) => {
-//     for (let thing of things) {
-//       const p = document.createElement("p");
-//       p.innerText = thing.num;
-//       document.body.append(p);
-//     }
-//   });
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ import express from "express";
 import pg from "pg";
 import dotenv from "dotenv";
 
+// Load enviroment variables from the .env file.
 dotenv.config();
 
+// Destructure out the enviroment variables we're interested in.
 const { PORT, DATABASE_URL } = process.env;
 
 const client = new pg.Client({
@@ -14,7 +16,10 @@ await client.connect();
 
 const app = express();
 
+// Host all files in the "public" directory at the root URL.
 app.use(express.static("public"));
+
+// Handle parsing incoming requests with JSON bodies.
 app.use(express.json());
 
 app.get("/things", (req, res) => {
@@ -25,13 +30,20 @@ app.get("/things", (req, res) => {
 
 app.post("/things", (req, res) => {
   const { num } = req.body;
-  console.log(req.body);
 
   client
     .query("INSERT INTO thing (num) VALUES ($1) RETURNING *", [num])
     .then((result) => {
       res.json(result.rows[0]);
     });
+});
+
+app.delete("/things/:id", (req, res) => {
+  const { id } = req.params;
+
+  client.query("DELETE FROM thing WHERE id = $1", [id]).then(() => {
+    res.sendStatus(204);
+  });
 });
 
 app.listen(PORT, () => {
